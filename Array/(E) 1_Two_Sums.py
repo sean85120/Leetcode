efe11fc697d1575b6b09c51fad533733b344950c
Sean <91415054+sean85120@users.noreply.github.com>
2025-02-11 23:51:04 +0800

    Add problem description in problem 1

--- Array/(E) 1_Two_Sums.py
+++ Array/(E) 1_Two_Sums.py
@@ -1,17 +1,29 @@
 from typing import List
 
-# O(n)
-def twoSum(nums: List[int], target: int) -> List[int]:
-    d = {}
-    for i, j in enumerate(nums):
+"""
+1. Two Sum
 
-        print(f"index:{i} value:{j}")
-        r = target - j
-        if r in d:
-            return [d[r], i]
-        d[j] = i
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+"""
+
+
+# O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, j in enumerate(nums):
+
+            print(f"index:{i} value:{j}")
+            r = target - j
+            if r in d:
+                return [d[r], i]
+            d[j] = i
 
 
 nums = [2, 7, 11, 13]
 target = 9
-twoSum(nums, target)
+print(Solution().twoSum(nums, target))
